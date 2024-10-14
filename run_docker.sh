7a2be3b98b3f222f753aaa50502c72cef2cd1fe9
Geon <114966864+geon0430@users.noreply.github.com>
2024-10-14 14:12:52 +0900

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
-port_num="5"
-CONTAINER_NAME="sung-ai-video-converter"
-IMAGE_NAME="hub.inbic.duckdns.org/ai-dev/ai-video-converter"
+port_num="1"
+CONTAINER_NAME="low-light-enhancement-training"
+IMAGE_NAME="low-light-enhancement-training"
 TAG="0.1"
 
-ai_video_converter_path=$(pwd)
+code_path=$(pwd)
 
 docker run \
     --runtime nvidia \
     --gpus all \
     -it \
-    -p ${port_num}8000:8000 \
     -p ${port_num}8888:8888 \
     --name ${CONTAINER_NAME} \
     --privileged \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -v /raid/datasets/LOLdataset:/volume/LOLdataset \
-    -v /raid/datasets/ImageNet:/volume/ImageNet \
-    -v ${ai_video_converter_path}:/ai-video-converter \
-    -v /home/ubuntu/geon/dataset:/volume \
+    # -v /datasets/LOLdataset:/volume/LOLdataset \
+    # -v /datasets/ImageNet:/volume/ImageNet \
+    -v ${code_path}:/lle \
     --shm-size 5g \
     --restart=always \
-    -w /ai-video-converter \
+    -w /lle \
     -e DISPLAY=$DISPLAY \
     ${IMAGE_NAME}:${TAG}
